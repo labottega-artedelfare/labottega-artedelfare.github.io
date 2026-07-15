9d65940b3e00001a05e16891ff4947f184219d2a
tracciagestionale <traccia.gestionale@gmail.com>
2026-07-16 00:13:34 +0200

    Configura link pubblici della Bottega

--- assets/js/config.js
+++ assets/js/config.js
@@ -2,10 +2,10 @@ window.SITE_CONFIG = Object.freeze({
   siteName: "La Bottega – Arte del Fare",
   contactEmail: "labottega.artedelfare@gmail.com",
 
-  // Inserire qui gli indirizzi pubblici definitivi.
-  websiteUrl: "",
-  instagramUrl: "",
-  youtubeUrl: "",
+  websiteUrl: "https://www.mimmociavarelli.com/",
+  instagramUrl: "https://www.instagram.com/artedelfare.labottega/",
+  youtubeUrl: "https://www.youtube.com/@MimmoCiavarelli",
+  facebookUrl: "https://www.facebook.com/profile.php?id=61582840887788&locale=it_IT",
 
   // Verrà valorizzato quando la pagina di registrazione sarà collegata
   // alla web app Google Apps Script.
@@ -13,5 +13,6 @@ window.SITE_CONFIG = Object.freeze({
 
   // Dati pubblici del programma sostenitori.
   registrationOpen: false,
+  badgeDuration: "annuale",
   badgeYear: "2026–2027"
 });
